1e1969d37d874e301c169429d56b8db897ca2457
Erik Thalén <erik@sorthvid.net>
2021-01-21 15:38:43 +0100

    init commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import './index.scss'
+
+// import * as dsb from '@dsb.dk/designsystem/dist/js/dsb.esm.js'
+// Object.values(dsb).forEach(module => typeof module === 'function' && module())
+
+import { defineExpandCollapse } from '@dsb.dk/designsystem/dist/js/dsb.esm.js'
+defineExpandCollapse()
