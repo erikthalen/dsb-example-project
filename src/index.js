4cdd543f6b2d4b1b04ec74345fbc1c0d77321029
Erik Thalén <erik@sorthvid.net>
2021-02-24 11:18:26 +0100

    init commit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import './index.scss'
 
+/**
+ * Get every custom element and define them
+ */
 // import * as dsb from '@dsb.dk/designsystem/dist/js/dsb.esm.js'
 // Object.values(dsb).forEach(module => typeof module === 'function' && module())
 
+/**
+ * Get single custom element and define it
+ */
 import { defineExpandCollapse } from '@dsb.dk/designsystem/dist/js/dsb.esm.js'
 defineExpandCollapse()
